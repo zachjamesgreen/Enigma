e63ff89efad80fb3c246c094ca8a719fab9fbf81
Zach Green <zachjamesgreen@gmail.com>
2021-04-22 12:57:10 -0600

    Add tests for encrypt/decrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,7 +4,25 @@ class Enigma
 
   def initialize; end
 
-  def encrypt(message, key, date); end
+  def encrypt(message, key = generate_key, date = formated_date)
+    message_array = message.split('')
 
-  def decrypt(ciphertext, key, date); end
+
+
+  end
+
+  def decrypt(ciphertext, key, date = Date.today); end
+
+  def generate_key
+    rand = Random.rand 100000
+    digits = rand.digits
+    until digits.size == 5 do
+      digits.unshift 0
+    end
+    digits.join('')
+  end
+
+  def formated_date
+    Date.today.strftime '%d%m%y'
+  end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,8 +1,31 @@
 require './lib/enigma'
 
 RSpec.describe Enigma do
-  it 'should exist' do
-    enigma = Enigma.new
-    expect(enigma).to be_instance_of(Enigma)
+  let(:enigma) { Enigma.new }
+  it 'should exist' { expect(enigma).to be_instance_of(Enigma) }
+
+  it 'should #encrypt' do
+    encrypted = enigma.encrypt("hello world", "02715", "040895")
+    expect(encrypted).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
+  end
+
+  it 'should #decrypt' do
+    encrypted = enigma.decrypt("hello world", "02715", "040895")
+    expect(encrypted).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
+  end
+
+  it 'should #encrypt using todays date' do
+    encrypted = enigma.encrypt("hello world", "02715")
+    expect(encrypted).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
+  end
+
+  it 'should #decrypt using todays date' do
+    encrypted = enigma.decrypt("hello world", "02715")
+    expect(encrypted).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
+  end
+
+  it 'should #encrypt using todays date and a random key' do
+    encrypted = enigma.encrypt("hello world")
+    expect(encrypted).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
   end
 end
